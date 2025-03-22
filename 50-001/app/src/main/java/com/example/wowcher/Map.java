a8d4d86888665b359c28663751c40e01cc2faab9
AndrewLJY <andrewloke9@gmail.com>
2025-03-22 23:45:57 +0800

    - Checks location is enabled - Search function - Retrieves surrounding places - Custom Markers (incomplete cause need voucher data) - Working on: filter function

--- 50-001/app/src/main/java/com/example/wowcher/Map.java
+++ 50-001/app/src/main/java/com/example/wowcher/Map.java
@@ -3,9 +3,11 @@ package com.example.wowcher;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
@@ -14,24 +16,46 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.AdvancedMarkerOptions;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapCapabilities;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PinConfig;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.AutocompletePrediction;
+import com.google.android.libraries.places.api.model.AutocompleteSessionToken;
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest;
+import com.google.android.libraries.places.api.net.PlacesClient;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -45,15 +69,22 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
 
     FusedLocationProviderClient fusedLocationProviderClient;
 
-    private CameraPosition cameraPosition;
     private LatLng userLocation;
+
+    private ArrayAdapter<String> autoCompleteAdapter;
+
+    private PlacesClient placesClient;
+
+    private AutocompleteSessionToken autocompleteSessionToken;
+
     private LatLng selectedLocation;
+
+    private boolean gpsEnabled = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.google_maps_test);
-
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
@@ -67,17 +98,77 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
             Places.initialize(this, apiKey);
         }
 
+        // Search Bar View
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.searchbar);
+
+        // Necessary variables for getting places from Google Maps
+        autoCompleteAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line);
+        placesClient = Places.createClient(this);
+        autocompleteSessionToken = AutocompleteSessionToken.newInstance();
+
+        // Handle when type in search bar
+        autoCompleteTextView.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String query = s.toString();
+                System.out.println("what");
+                if (!query.isEmpty()) {
+                    System.out.println(query);
+                    // Build a request to fetch autocomplete predictions based on the user's input
+                    FindAutocompletePredictionsRequest request = FindAutocompletePredictionsRequest.builder()
+                            .setSessionToken(autocompleteSessionToken)
+                            .setQuery(query)
+                            .build();
+
+                    // Fetch autocomplete predictions from the Places API
+                    placesClient.findAutocompletePredictions(request).addOnSuccessListener(response -> {
+                        autoCompleteAdapter.clear(); // Clear the previous suggestions
+                        for (AutocompletePrediction prediction : response.getAutocompletePredictions()) {
+                            // Add each prediction to the adapter for displaying in the dropdown
+                            autoCompleteAdapter.add(prediction.getFullText(null).toString());
+                        }
+                        autoCompleteAdapter.notifyDataSetChanged(); // Notify the adapter to update the dropdown
+                    });
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+        });
+
+        autoCompleteTextView.setAdapter(autoCompleteAdapter);
+
+        // Set an item click listener to handle when the user selects a suggestion
+        autoCompleteTextView.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedPlace = autoCompleteAdapter.getItem(position);
+            if (selectedPlace != null) {
+                // Call the callback function to handle the selected place
+                selectLocation(selectedPlace, this);
+            }
+        });
+
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
 
+        checkLocationEnabled();
+
         // Check for location permissions
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // If don't have, request for permissions
             requestPermissions(new String[]{
-                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.ACCESS_COARSE_LOCATION
             }, REQUEST_CODE_ASK_PERMISSIONS);
         } else {
@@ -85,6 +176,22 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
             fetchUserLocation(this, fusedLocationProviderClient);
         }
 
+    }
+
+    // Check if user location service is enabled
+    public void checkLocationEnabled() {
+        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        try {
+            gpsEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception ex) {
+            Log.e("LOCATION SERVICES ERROR", "ERROR: " + ex);
+        }
+
+        if (!gpsEnabled) {
+            // Notify user
+            Toast.makeText(this, "Please enable location services.", Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 
     // Function to fetch the user's location
@@ -99,17 +206,9 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
                         userLocation = new LatLng(location.getLatitude(), location.getLongitude());
 
                         // Initialize camera position of user location
-                        cameraPosition = CameraPosition.fromLatLngZoom(userLocation, 20f);
+                        CameraPosition cameraPosition = CameraPosition.fromLatLngZoom(userLocation, 20f);
 
-                        // Move camera to user location
-                        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-                        // Add marker to user location
-                        mMap.addMarker(
-                                new MarkerOptions()
-                                        .position(userLocation)
-
-                        );
+                        moveCameraToLocation(cameraPosition);
 
                         // Enable necessary map functions
                         enableMapsSettings();
@@ -125,23 +224,33 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
         }
     }
 
-    // Handles selected place from the search results - TODO
-    public void selectLocation(String selectedPlace, Context context){
-        Geocoder geocoder = new Geocoder(context);
-        try {
-            List<Address> addressList = geocoder.getFromLocationName(selectedPlace, 1);
-            if(!addressList.isEmpty()){
-                Address address = addressList.get(0);
-                selectedLocation = new LatLng(address.getLatitude(), address.getLongitude());
-            }else{
-                Toast.makeText(this, "No location found for the selected place.", Toast.LENGTH_LONG)
-                        .show();
-            }
-        }catch(IOException e){
-            Log.e("Get addresses fail", "Error: "+e);
+    // Add marker to user location
+    public void addMarkerToMap(LatLng location, int imageId) {
+        MarkerOptions markerOptions = new MarkerOptions()
+                .position(location)
+                .icon(BitmapDescriptorFactory.fromBitmap(customMarker(imageId)))
+                .flat(true);
+        mMap.addMarker(markerOptions);
+    }
+
+    // Create custom marker from image from drawable
+    private Bitmap customMarker(int resId) {
+        Bitmap bitmap = null;
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(), resId, null);
+        if (drawable != null) {
+            bitmap = Bitmap.createBitmap(200, 250, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
         }
 
+        return bitmap;
+    }
 
+    // Move camera to user location
+    public void moveCameraToLocation(CameraPosition cameraPosition) {
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 
     // Suppressed permission check since this will only be used after permission is checked
@@ -184,4 +293,34 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
             return null;
         }
     }
+
+    // Handle selecting location from recommended search
+    public void selectLocation(String selectedPlace, Context context) {
+        Geocoder geocoder = new Geocoder(context);
+        try {
+            // Obtain list of addresses in Google Maps from selected place
+            List<Address> addressList = geocoder.getFromLocationName(selectedPlace, 1);
+
+            // Handle if selected location is found
+            if (!addressList.isEmpty()) {
+                Address address = addressList.get(0);
+
+                selectedLocation = new LatLng(address.getLatitude(), address.getLongitude());
+
+                CameraPosition cameraPosition = CameraPosition.fromLatLngZoom(selectedLocation, 20f);
+                moveCameraToLocation(cameraPosition);
+
+            }
+            // Handle if selected location is not found
+            else {
+                Toast.makeText(this, "No location found for the selected place.", Toast.LENGTH_LONG)
+                        .show();
+            }
+        } catch (IOException e) {
+            Log.e("Get addresses fail", "Error: " + e);
+        }
+
+
+    }
+
 }
