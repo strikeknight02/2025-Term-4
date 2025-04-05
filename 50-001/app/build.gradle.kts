37da31ebcdc699e8b1615aaec115be061cb174c6
Your Name <michael.rasco24@gmail.com>
2025-04-05 14:48:15 +0800

    login+fyp

--- 50-001/app/build.gradle.kts
+++ 50-001/app/build.gradle.kts
@@ -44,4 +44,6 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation(libs.firebase.analytics)
+
+
 }
---
+++ 50-001/app/src/main/java/com/example/wowcher/Login.java
@@ -0,0 +1,4 @@
+package com.example.wowcher;
+
+public class Login {
+}
